61b11451de80a07c411ef0e2ad6327450c18ba2b
Brunoiooo <applefix@applefix.smarthost.pl>
2024-10-08 01:04:05 +0200

    Refactor MenuForm

--- MenuForm.cpp
+++ MenuForm.cpp
@@ -1,9 +1,26 @@
 #include "MenuForm.h"
+#include "GameForm.h"
+#include <stdexcept>
 
 namespace oczko {
-	void MenuForm::MenuForm_Load(System::Object^ sender, System::EventArgs^ e)
+	//Handlers
+	System::Void MenuForm::StartButton_Click(System::Object^ sender, System::EventArgs^ e)
 	{
-		this->WindowState = System::Windows::Forms::FormWindowState::Maximized;
-		MessageBox::Show("Form is Loaded");
+		try {
+			float startMoney = Single::Parse(this->StartMoneyTextBox->Text);
+
+			if (startMoney <= 0) throw gcnew System::Exception("Start money has to be bigger than 0.");
+
+			this->Hide();
+			GameForm^ gameForm = gcnew GameForm(startMoney);
+			gameForm->Show();
+		}
+		catch (System::Exception^ e) {
+			MessageBox::Show(e->Message);
+		}
+		
+		return System::Void();
 	}
 }
+
+
--- MenuForm.h
+++ MenuForm.h
@@ -9,24 +9,18 @@ namespace oczko {
 	using namespace System::Data;
 	using namespace System::Drawing;
 
-	/// <summary>
-	/// Summary for MenuForm
-	/// </summary>
 	public ref class MenuForm : public System::Windows::Forms::Form
 	{
+		//Constructors
 	public:
 		MenuForm(void)
 		{
 			InitializeComponent();
-			//
-			//TODO: Add the constructor code here
-			//
+			this->WindowState = System::Windows::Forms::FormWindowState::Maximized;
 		}
 
+		//Destructor
 	protected:
-		/// <summary>
-		/// Clean up any resources being used.
-		/// </summary>
 		~MenuForm()
 		{
 			if (components)
@@ -35,14 +29,18 @@ namespace oczko {
 			}
 		}
 
-	protected:
+		//Handlers
+	private: 
+		System::Void StartButton_Click(System::Object^ sender, System::EventArgs^ e);
+
+		//Componenst
+	private: 
+		System::Windows::Forms::Button^ StartButton;
+		System::Windows::Forms::TextBox^ StartMoneyTextBox;
 
 	private:
-		/// <summary>
-		/// Required designer variable.
-		/// </summary>
 		System::ComponentModel::Container ^components;
-		void MenuForm_Load(System::Object^ sender, System::EventArgs^ e);
+
 #pragma region Windows Form Designer generated code
 		/// <summary>
 		/// Required method for Designer support - do not modify
@@ -50,19 +48,39 @@ namespace oczko {
 		/// </summary>
 		void InitializeComponent(void)
 		{
-			System::ComponentModel::ComponentResourceManager^ resources = (gcnew System::ComponentModel::ComponentResourceManager(MenuForm::typeid));
+			this->StartButton = (gcnew System::Windows::Forms::Button());
+			this->StartMoneyTextBox = (gcnew System::Windows::Forms::TextBox());
 			this->SuspendLayout();
+			// 
+			// StartButton
+			// 
+			this->StartButton->Location = System::Drawing::Point(723, 577);
+			this->StartButton->Name = L"StartButton";
+			this->StartButton->Size = System::Drawing::Size(240, 101);
+			this->StartButton->TabIndex = 0;
+			this->StartButton->Text = L"Start";
+			this->StartButton->UseVisualStyleBackColor = true;
+			this->StartButton->Click += gcnew System::EventHandler(this, &MenuForm::StartButton_Click);
+			// 
+			// StartMoneyTextBox
+			// 
+			this->StartMoneyTextBox->Location = System::Drawing::Point(723, 209);
+			this->StartMoneyTextBox->Name = L"StartMoneyTextBox";
+			this->StartMoneyTextBox->Size = System::Drawing::Size(216, 22);
+			this->StartMoneyTextBox->TabIndex = 1;
+			this->StartMoneyTextBox->Text = L"1000";
 			// 
 			// MenuForm
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(1900, 862);
-			this->Icon = (cli::safe_cast<System::Drawing::Icon^>(resources->GetObject(L"$this.Icon")));
+			this->ClientSize = System::Drawing::Size(1742, 833);
+			this->Controls->Add(this->StartMoneyTextBox);
+			this->Controls->Add(this->StartButton);
 			this->Name = L"MenuForm";
 			this->Text = L"MenuForm";
-			this->Load += gcnew System::EventHandler(this, &MenuForm::MenuForm_Load);
 			this->ResumeLayout(false);
+			this->PerformLayout();
 
 		}
 #pragma endregion
