47232f8936e8717377d2cecad6da411212daa37b
Brunoiooo <applefix@applefix.smarthost.pl>
2024-10-08 20:32:21 +0200

    Dodanie wymagabych this

--- GameForm.h
+++ GameForm.h
@@ -113,173 +113,173 @@ namespace oczko {
 #pragma region Windows Form Designer generated code
 		void InitializeComponent(void)
 		{
-			HandsListBox = (gcnew System::Windows::Forms::ListBox());
-			BetTextBox = (gcnew System::Windows::Forms::TextBox());
-			NewBetButton = (gcnew System::Windows::Forms::Button());
-			CoupierHandListBox = (gcnew System::Windows::Forms::ListBox());
-			PlayerHandListBox = (gcnew System::Windows::Forms::ListBox());
-			HitButton = (gcnew System::Windows::Forms::Button());
-			StandButton = (gcnew System::Windows::Forms::Button());
-			DoubleButton = (gcnew System::Windows::Forms::Button());
-			SplitButton = (gcnew System::Windows::Forms::Button());
-			BetLabel = (gcnew System::Windows::Forms::Label());
-			MultiplierLabel = (gcnew System::Windows::Forms::Label());
-			CardCountLabel = (gcnew System::Windows::Forms::Label());
-			SplitBoxListBox = (gcnew System::Windows::Forms::ListBox());
-			MoneyLabel = (gcnew System::Windows::Forms::Label());
-			SuspendLayout();
+			this->HandsListBox = (gcnew System::Windows::Forms::ListBox());
+			this->BetTextBox = (gcnew System::Windows::Forms::TextBox());
+			this->NewBetButton = (gcnew System::Windows::Forms::Button());
+			this->CoupierHandListBox = (gcnew System::Windows::Forms::ListBox());
+			this->PlayerHandListBox = (gcnew System::Windows::Forms::ListBox());
+			this->HitButton = (gcnew System::Windows::Forms::Button());
+			this->StandButton = (gcnew System::Windows::Forms::Button());
+			this->DoubleButton = (gcnew System::Windows::Forms::Button());
+			this->SplitButton = (gcnew System::Windows::Forms::Button());
+			this->BetLabel = (gcnew System::Windows::Forms::Label());
+			this->MultiplierLabel = (gcnew System::Windows::Forms::Label());
+			this->CardCountLabel = (gcnew System::Windows::Forms::Label());
+			this->SplitBoxListBox = (gcnew System::Windows::Forms::ListBox());
+			this->MoneyLabel = (gcnew System::Windows::Forms::Label());
+			this->SuspendLayout();
 			// 
 			// HandsListBox
 			// 
-			HandsListBox->FormattingEnabled = true;
-			HandsListBox->ItemHeight = 16;
-			HandsListBox->Location = System::Drawing::Point(12, 12);
-			HandsListBox->Name = L"HandsListBox";
-			HandsListBox->Size = System::Drawing::Size(144, 164);
-			HandsListBox->TabIndex = 0;
-			HandsListBox->SelectedIndexChanged += gcnew System::EventHandler(this, &GameForm::HandsListBox_SelectedIndexChanged);
+			this->HandsListBox->FormattingEnabled = true;
+			this->HandsListBox->ItemHeight = 16;
+			this->HandsListBox->Location = System::Drawing::Point(12, 12);
+			this->HandsListBox->Name = L"HandsListBox";
+			this->HandsListBox->Size = System::Drawing::Size(144, 164);
+			this->HandsListBox->TabIndex = 0;
+			this->HandsListBox->SelectedIndexChanged += gcnew System::EventHandler(this, &GameForm::HandsListBox_SelectedIndexChanged);
 			// 
 			// BetTextBox
 			// 
-			BetTextBox->Location = System::Drawing::Point(12, 182);
-			BetTextBox->Name = L"BetTextBox";
-			BetTextBox->Size = System::Drawing::Size(100, 22);
-			BetTextBox->TabIndex = 1;
-			BetTextBox->Text = L"100";
+			this->BetTextBox->Location = System::Drawing::Point(12, 182);
+			this->BetTextBox->Name = L"BetTextBox";
+			this->BetTextBox->Size = System::Drawing::Size(100, 22);
+			this->BetTextBox->TabIndex = 1;
+			this->BetTextBox->Text = L"100";
 			// 
 			// NewBetButton
 			// 
-			NewBetButton->Location = System::Drawing::Point(12, 210);
-			NewBetButton->Name = L"NewBetButton";
-			NewBetButton->Size = System::Drawing::Size(75, 23);
-			NewBetButton->TabIndex = 2;
-			NewBetButton->Text = L"New Bet";
-			NewBetButton->UseVisualStyleBackColor = true;
-			NewBetButton->Click += gcnew System::EventHandler(this, &GameForm::NewBetButton_Click);
+			this->NewBetButton->Location = System::Drawing::Point(12, 210);
+			this->NewBetButton->Name = L"NewBetButton";
+			this->NewBetButton->Size = System::Drawing::Size(75, 23);
+			this->NewBetButton->TabIndex = 2;
+			this->NewBetButton->Text = L"New Bet";
+			this->NewBetButton->UseVisualStyleBackColor = true;
+			this->NewBetButton->Click += gcnew System::EventHandler(this, &GameForm::NewBetButton_Click);
 			// 
 			// CoupierHandListBox
 			// 
-			CoupierHandListBox->FormattingEnabled = true;
-			CoupierHandListBox->ItemHeight = 16;
-			CoupierHandListBox->Location = System::Drawing::Point(1617, 12);
-			CoupierHandListBox->Name = L"CoupierHandListBox";
-			CoupierHandListBox->Size = System::Drawing::Size(158, 180);
-			CoupierHandListBox->TabIndex = 3;
+			this->CoupierHandListBox->FormattingEnabled = true;
+			this->CoupierHandListBox->ItemHeight = 16;
+			this->CoupierHandListBox->Location = System::Drawing::Point(1617, 12);
+			this->CoupierHandListBox->Name = L"CoupierHandListBox";
+			this->CoupierHandListBox->Size = System::Drawing::Size(158, 180);
+			this->CoupierHandListBox->TabIndex = 3;
 			// 
 			// PlayerHandListBox
 			// 
-			PlayerHandListBox->FormattingEnabled = true;
-			PlayerHandListBox->ItemHeight = 16;
-			PlayerHandListBox->Location = System::Drawing::Point(1453, 12);
-			PlayerHandListBox->Name = L"PlayerHandListBox";
-			PlayerHandListBox->Size = System::Drawing::Size(158, 180);
-			PlayerHandListBox->TabIndex = 4;
+			this->PlayerHandListBox->FormattingEnabled = true;
+			this->PlayerHandListBox->ItemHeight = 16;
+			this->PlayerHandListBox->Location = System::Drawing::Point(1453, 12);
+			this->PlayerHandListBox->Name = L"PlayerHandListBox";
+			this->PlayerHandListBox->Size = System::Drawing::Size(158, 180);
+			this->PlayerHandListBox->TabIndex = 4;
 			// 
 			// HitButton
 			// 
-			HitButton->Location = System::Drawing::Point(710, 538);
-			HitButton->Name = L"HitButton";
-			HitButton->Size = System::Drawing::Size(75, 23);
-			HitButton->TabIndex = 5;
-			HitButton->Text = L"Hit";
-			HitButton->UseVisualStyleBackColor = true;
+			this->HitButton->Location = System::Drawing::Point(710, 538);
+			this->HitButton->Name = L"HitButton";
+			this->HitButton->Size = System::Drawing::Size(75, 23);
+			this->HitButton->TabIndex = 5;
+			this->HitButton->Text = L"Hit";
+			this->HitButton->UseVisualStyleBackColor = true;
 			// 
 			// StandButton
 			// 
-			StandButton->Location = System::Drawing::Point(791, 538);
-			StandButton->Name = L"StandButton";
-			StandButton->Size = System::Drawing::Size(75, 23);
-			StandButton->TabIndex = 6;
-			StandButton->Text = L"Stand";
-			StandButton->UseVisualStyleBackColor = true;
+			this->StandButton->Location = System::Drawing::Point(791, 538);
+			this->StandButton->Name = L"StandButton";
+			this->StandButton->Size = System::Drawing::Size(75, 23);
+			this->StandButton->TabIndex = 6;
+			this->StandButton->Text = L"Stand";
+			this->StandButton->UseVisualStyleBackColor = true;
 			// 
 			// DoubleButton
 			// 
-			DoubleButton->Location = System::Drawing::Point(872, 538);
-			DoubleButton->Name = L"DoubleButton";
-			DoubleButton->Size = System::Drawing::Size(75, 23);
-			DoubleButton->TabIndex = 7;
-			DoubleButton->Text = L"Double";
-			DoubleButton->UseVisualStyleBackColor = true;
+			this->DoubleButton->Location = System::Drawing::Point(872, 538);
+			this->DoubleButton->Name = L"DoubleButton";
+			this->DoubleButton->Size = System::Drawing::Size(75, 23);
+			this->DoubleButton->TabIndex = 7;
+			this->DoubleButton->Text = L"Double";
+			this->DoubleButton->UseVisualStyleBackColor = true;
 			// 
 			// SplitButton
 			// 
-			SplitButton->Location = System::Drawing::Point(953, 538);
-			SplitButton->Name = L"SplitButton";
-			SplitButton->Size = System::Drawing::Size(75, 23);
-			SplitButton->TabIndex = 8;
-			SplitButton->Text = L"Split";
-			SplitButton->UseVisualStyleBackColor = true;
+			this->SplitButton->Location = System::Drawing::Point(953, 538);
+			this->SplitButton->Name = L"SplitButton";
+			this->SplitButton->Size = System::Drawing::Size(75, 23);
+			this->SplitButton->TabIndex = 8;
+			this->SplitButton->Text = L"Split";
+			this->SplitButton->UseVisualStyleBackColor = true;
 			// 
 			// BetLabel
 			// 
-			BetLabel->AutoSize = true;
-			BetLabel->Location = System::Drawing::Point(549, 45);
-			BetLabel->Name = L"BetLabel";
-			BetLabel->Size = System::Drawing::Size(27, 16);
-			BetLabel->TabIndex = 9;
-			BetLabel->Text = L"Bet";
+			this->BetLabel->AutoSize = true;
+			this->BetLabel->Location = System::Drawing::Point(549, 45);
+			this->BetLabel->Name = L"BetLabel";
+			this->BetLabel->Size = System::Drawing::Size(27, 16);
+			this->BetLabel->TabIndex = 9;
+			this->BetLabel->Text = L"Bet";
 			// 
 			// MultiplierLabel
 			// 
-			MultiplierLabel->AutoSize = true;
-			MultiplierLabel->Location = System::Drawing::Point(589, 45);
-			MultiplierLabel->Name = L"MultiplierLabel";
-			MultiplierLabel->Size = System::Drawing::Size(60, 16);
-			MultiplierLabel->TabIndex = 10;
-			MultiplierLabel->Text = L"Multiplier";
-			MultiplierLabel->TextAlign = System::Drawing::ContentAlignment::TopCenter;
+			this->MultiplierLabel->AutoSize = true;
+			this->MultiplierLabel->Location = System::Drawing::Point(589, 45);
+			this->MultiplierLabel->Name = L"MultiplierLabel";
+			this->MultiplierLabel->Size = System::Drawing::Size(60, 16);
+			this->MultiplierLabel->TabIndex = 10;
+			this->MultiplierLabel->Text = L"Multiplier";
+			this->MultiplierLabel->TextAlign = System::Drawing::ContentAlignment::TopCenter;
 			// 
 			// CardCountLabel
 			// 
-			CardCountLabel->AutoSize = true;
-			CardCountLabel->Location = System::Drawing::Point(1359, 12);
-			CardCountLabel->Name = L"CardCountLabel";
-			CardCountLabel->Size = System::Drawing::Size(73, 16);
-			CardCountLabel->TabIndex = 11;
-			CardCountLabel->Text = L"Card Count";
+			this->CardCountLabel->AutoSize = true;
+			this->CardCountLabel->Location = System::Drawing::Point(1359, 12);
+			this->CardCountLabel->Name = L"CardCountLabel";
+			this->CardCountLabel->Size = System::Drawing::Size(73, 16);
+			this->CardCountLabel->TabIndex = 11;
+			this->CardCountLabel->Text = L"Card Count";
 			// 
 			// SplitBoxListBox
 			// 
-			SplitBoxListBox->FormattingEnabled = true;
-			SplitBoxListBox->ItemHeight = 16;
-			SplitBoxListBox->Location = System::Drawing::Point(1034, 538);
-			SplitBoxListBox->Name = L"SplitBoxListBox";
-			SplitBoxListBox->Size = System::Drawing::Size(158, 180);
-			SplitBoxListBox->TabIndex = 12;
+			this->SplitBoxListBox->FormattingEnabled = true;
+			this->SplitBoxListBox->ItemHeight = 16;
+			this->SplitBoxListBox->Location = System::Drawing::Point(1034, 538);
+			this->SplitBoxListBox->Name = L"SplitBoxListBox";
+			this->SplitBoxListBox->Size = System::Drawing::Size(158, 180);
+			this->SplitBoxListBox->TabIndex = 12;
 			// 
 			// MoneyLabel
 			// 
-			MoneyLabel->AutoSize = true;
-			MoneyLabel->Location = System::Drawing::Point(162, 9);
-			MoneyLabel->Name = L"MoneyLabel";
-			MoneyLabel->Size = System::Drawing::Size(48, 16);
-			MoneyLabel->TabIndex = 13;
-			MoneyLabel->Text = L"Money";
+			this->MoneyLabel->AutoSize = true;
+			this->MoneyLabel->Location = System::Drawing::Point(162, 9);
+			this->MoneyLabel->Name = L"MoneyLabel";
+			this->MoneyLabel->Size = System::Drawing::Size(48, 16);
+			this->MoneyLabel->TabIndex = 13;
+			this->MoneyLabel->Text = L"Money";
 			// 
 			// GameForm
 			// 
-			AutoScaleDimensions = System::Drawing::SizeF(8, 16);
-			AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			ClientSize = System::Drawing::Size(1787, 777);
-			Controls->Add(MoneyLabel);
-			Controls->Add(SplitBoxListBox);
-			Controls->Add(CardCountLabel);
-			Controls->Add(MultiplierLabel);
-			Controls->Add(BetLabel);
-			Controls->Add(SplitButton);
-			Controls->Add(DoubleButton);
-			Controls->Add(StandButton);
-			Controls->Add(HitButton);
-			Controls->Add(PlayerHandListBox);
-			Controls->Add(CoupierHandListBox);
-			Controls->Add(NewBetButton);
-			Controls->Add(BetTextBox);
-			Controls->Add(HandsListBox);
-			Name = L"GameForm";
-			Text = L"GameForm";
-			ResumeLayout(false);
-			PerformLayout();
+			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(1787, 777);
+			this->Controls->Add(MoneyLabel);
+			this->Controls->Add(SplitBoxListBox);
+			this->Controls->Add(CardCountLabel);
+			this->Controls->Add(MultiplierLabel);
+			this->Controls->Add(BetLabel);
+			this->Controls->Add(SplitButton);
+			this->Controls->Add(DoubleButton);
+			this->Controls->Add(StandButton);
+			this->Controls->Add(HitButton);
+			this->Controls->Add(PlayerHandListBox);
+			this->Controls->Add(CoupierHandListBox);
+			this->Controls->Add(NewBetButton);
+			this->Controls->Add(BetTextBox);
+			this->Controls->Add(HandsListBox);
+			this->Name = L"GameForm";
+			this->Text = L"GameForm";
+			this->ResumeLayout(false);
+			this->PerformLayout();
 
 		}
 #pragma endregion
