b05801900f327edd59c273595f42057b8734d743
Brunoiooo <applefix@applefix.smarthost.pl>
2024-10-08 19:58:55 +0200

    Delete this from Deck.cpp

--- Deck.cpp
+++ Deck.cpp
@@ -4,28 +4,29 @@
 namespace Deck {
 	//Methods
 	Card::Card^ Deck::Draw() {
-		if (this->Cards->Count < 1) throw std::runtime_error("Desk is empty.");
+		if (Cards->Count < 1) throw std::runtime_error("Desk is empty.");
 
 		System::Random^ random = gcnew System::Random();
-		int index = random->Next(0, this->Cards->Count - 1);
+		int index = random->Next(0, Cards->Count - 1);
 
-		Card::Card^ card = this->Cards[index];
+		Card::Card^ card = Cards[index];
+		Cards->Remove(card);
 
 		return card;
 	}
 
 	unsigned int Deck::GetCountOfCards() {
-		return this->Cards->Count;
+		return Cards->Count;
 	}
 
 	//Constructors
 	Deck::Deck() {
-		this->Cards = gcnew List<Card::Card^>();
+		Cards = gcnew List<Card::Card^>();
 
 		for (unsigned int i = 1; i <= 4; i++)
 		{
 			for (unsigned int x = 2; x <= 11; x++) {
-				this->Cards->Add(gcnew Card::Card(i, x));
+				Cards->Add(gcnew Card::Card(i, x));
 			}
 		}
 	}
