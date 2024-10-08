9b53c0f0f945ca0f958df8d8c9ba13042fb7efe0
Brunoiooo <applefix@applefix.smarthost.pl>
2024-10-08 19:58:24 +0200

    delete this from Card.cpp

--- Card.cpp
+++ Card.cpp
@@ -4,11 +4,11 @@
 namespace Card {
 	//Methods
 	unsigned int Card::GetColor() {
-		return this->Color;
+		return Color;
 	}
 
 	unsigned int Card::GetValue() {
-		return this->Value;
+		return Value;
 	}
 
 	//Constructors
@@ -16,7 +16,7 @@ namespace Card {
 		if (color < 1 || 4 < color) throw std::runtime_error("Card's color has to be between 1 and 4.");
 		if (value < 2 || 11 < value) throw std::runtime_error("Card's value has to be between 21 and 11.");
 
-		this->Color = color;
-		this->Value = value;
-	};
+		Color = color;
+		Value = value;
+	}
 }
