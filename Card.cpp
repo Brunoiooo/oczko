f1cea2d5038b2647db212cbc07f08e0ddf02cfa0
Brunoiooo <applefix@applefix.smarthost.pl>
2024-10-09 23:55:09 +0200

    Change Card's class

--- Card.cpp
+++ Card.cpp
@@ -2,7 +2,6 @@
 #include <stdexcept>
 
 namespace Card {
-	//Methods
 	unsigned int Card::GetColor() {
 		return Color;
 	}
@@ -11,11 +10,7 @@ namespace Card {
 		return Value;
 	}
 
-	//Constructors
 	Card::Card(unsigned int color, unsigned int value) {
-		if (color < 1 || 4 < color) throw std::runtime_error("Card's color has to be between 1 and 4.");
-		if (value < 2 || 11 < value) throw std::runtime_error("Card's value has to be between 21 and 11.");
-
 		Color = color;
 		Value = value;
 	}
--- Card.h
+++ Card.h
@@ -2,17 +2,14 @@
 
 namespace Card {
 	public ref class Card {
-		//Models
 	private:
 		unsigned int Color;
 		unsigned int Value;
 
-		//Methods
 	public:
 		unsigned int GetColor();
 		unsigned int GetValue();
 
-		//Constructors
 	public:
 		Card(unsigned int color, unsigned int value);
 	};
