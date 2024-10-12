3675023984d5996ad18a5622be9c012a4c8bab64
Brunoiooo <applefix@applefix.smarthost.pl>
2024-10-12 14:39:18 +0200

    Fix

--- Bet.cpp
+++ Bet.cpp
@@ -1,129 +1,123 @@
-#include "Bet.h"
+#include "Bet.hpp"
 
 namespace Bet {
-	Bet::Bet(float baseBet, Hand::Hand^ croupierHand, Hand::Hand^ playerHand)
-	{
-		BaseBet = baseBet >= 0 ? baseBet : 0;
-		Stop = false;
-		Hit = false;
-		PlayerHand = playerHand;
-		CroupierHand = croupierHand;
-	}
-	Hand::Hand^ Bet::GetPlayerHand()
-	{
-		return PlayerHand;
-	}
-	Hand::Hand^ Bet::GetCroupierHand()
-	{
-		return CroupierHand;
-	}
 	float Bet::GetBaseBet()
 	{
 		return BaseBet;
 	}
-	void Bet::PlayHit(Card::Card^ card)
-	{
-		if (Stop)
-			throw gcnew System::Exception("Game is stopped.");
-
-		Hit = true;
-
-		AddPlayerCard(card);
-	}
-
-	bool Bet::IsHit()
-	{
-		return Hit;
-	}
-
-	void Bet::PlayStand()
-	{
-		Stop = true;
-	}
-
-	void Bet::PlayDouble(Card::Card^ card)
-	{
-		if (Stop)
-			throw gcnew System::Exception("Game is stopped.");
-		if (Hit)
-			throw gcnew System::Exception("Hit had played.");
-
-		Stop = true;
-		BaseBet += BaseBet;
-		AddPlayerCard(card);
-	}
-
-	Card::Card^ Bet::PlaySplit()
-	{
-		if(!CanSplit())
-			throw gcnew System::Exception("Split can't be used.");
-
-		return PlayerHand->StillCard(PlayerHand->GetCards()[0]);
-
-	}
-
-	bool Bet::CanSplit()
-	{
-		return (Stop ||
-			Hit ||
-			PlayerHand->GetCards()->Count != 2 ||
-			(PlayerHand->GetCards()[0]->GetValue() != PlayerHand->GetCards()[1]->GetValue())) ? false : true;
-	}
-	
 	float Bet::GetMultiplier()
 	{
-		return (PlayerHand->GetScore() == 21 || PlayerHand->GetScore() == 22) && PlayerHand->GetCards()->Count == 2 ? 2.5 : 2;
+		return PlayerHand->GetScore() >= 21 && PlayerHand->GetSize() == 2 && !IsSplited ? 2.5 : 2;
+	}
+	void Bet::Hit()
+	{
+		if (!CanHit())
+			throw new runtime_error("Hit can't be used!");
+
+		PlayerHand->AddCard(Croupier->DrawCard());
+		IsHitted = true;
+	}
+	bool Bet::CanHit()
+	{
+		return !IsStop();
+	}
+	void Bet::Stand()
+	{
+		if (!CanStand())
+			throw new runtime_error("Stand can't be used!");
+
+		IsStanded = true;
+	}
+	bool Bet::CanStand()
+	{
+		return !IsStop();
+	}
+	void Bet::Double(Player::Player* player)
+	{
+		if (!CanDouble(player))
+			throw new runtime_error("Double can't be used!");
+
+		PlayerHand->AddCard(Croupier->DrawCard());
+		player->CanWithdrawal(BaseBet);
+		BaseBet *= 2;
+		IsDoubled = true;
+	}
+	bool Bet::CanDouble(Player::Player* player)
+	{
+		return player->CanWithdrawal(BaseBet) && !IsStop() && !IsHitted;
+	}
+	Bet* Bet::Split(Player::Player* player)
+	{
+		if (!CanSplit(player))
+			throw new runtime_error("Split can't be used!");
+
+		Bet* bet = new Bet(BaseBet, PlayerHand->Split(), Croupier, true);
+
+		player->Withdrawal(BaseBet);
+		IsSplited = true;
+
+		return bet;
+	}
+	bool Bet::CanSplit(Player::Player* player)
+	{
+		return player->CanWithdrawal(BaseBet) && !IsSplited && !IsStop() && PlayerHand->CanSplit();
 	}
 	bool Bet::IsStop()
 	{
-		return Stop;
-	}
-	void Bet::AddPlayerCard(Card::Card^ card)
-	{
-		PlayerHand->AddCart(card);
+		if (IsStanded)
+			return true;
+
+		if (IsDoubled)
+			return true;
+
+		if (PlayerHand->GetSize() == 2 && !IsSplited) 
+			return false;
 		
-		if (PlayerHand->GetScore() > 21 && PlayerHand->GetCards()->Count > 2) Stop = true;
+		if (PlayerHand->GetScore() > 21)
+			return true;
+
+		return false;
 	}
 	bool Bet::IsWin()
 	{
-		if (!Stop) 
+		if (!IsStop())
 			return false;
 
-		if (PlayerHand->GetScore() > 21 && PlayerHand->GetCards()->Count > 2)
+		if (PlayerHand->GetScore() > 21 && PlayerHand->GetSize() > 2)
 			return false;
 
-		if (CroupierHand->GetScore() > 21 && CroupierHand->GetCards()->Count > 2)
+		if (Croupier->GetCroupierHand()->GetScore() > 21 && Croupier->GetCroupierHand()->GetSize() > 2)
 			return true;
 
-		if (CroupierHand->GetScore() < PlayerHand->GetScore())
+		if (Croupier->GetCroupierHand()->GetScore() < PlayerHand->GetScore())
 			return true;
 
 		return false;
 	}
 	bool Bet::IsLost()
 	{
-		if (!Stop)
+		if (!IsStop())
 			return false;
 
-		if (PlayerHand->GetScore() > 21 && PlayerHand->GetCards()->Count > 2)
+		if (PlayerHand->GetScore() > 21 && PlayerHand->GetSize() > 2)
 			return true;
 
-		if (CroupierHand->GetScore() > 21 && CroupierHand->GetCards()->Count > 2)
+		if (Croupier->GetCroupierHand()->GetScore() > 21 && Croupier->GetCroupierHand()->GetSize() > 2)
 			return false;
 
-		if (CroupierHand->GetScore() > PlayerHand->GetScore())
+		if (Croupier->GetCroupierHand()->GetScore() > PlayerHand->GetScore())
 			return true;
 
 		return false;
 	}
 	bool Bet::IsDraw()
 	{
-		if (!Stop)
+		if (!IsStop())
 			return false;
 
 		return !(IsWin() || IsLost());
 	}
-	float Bet::Payout()
+	float Bet::GetPay()
 	{
 		if (!IsStop())
 			return 0;
@@ -135,5 +129,10 @@ namespace Bet {
 			return BaseBet;
 
 		return 0;
+	}
+	Bet::~Bet()
+	{
+		delete PlayerHand;
+		delete Croupier;
 	}
 }
---
+++ Bet.hpp
@@ -0,0 +1,69 @@
+#pragma once
+
+#include "Hand.hpp"
+#include "Card.hpp"
+#include "Croupier.hpp"
+#include "Player.hpp"
+
+namespace Bet {
+	using namespace std;
+
+	class Bet {
+	private:
+		float BaseBet;
+		Hand::Hand* PlayerHand;
+		Croupier::Croupier* Croupier;
+		bool IsHitted;
+		bool IsStanded;
+		bool IsDoubled;
+		bool IsSplited;
+
+	public:
+		Bet(float baseBet, Hand::Hand* playerHand, Croupier::Croupier* croupier) : BaseBet(baseBet), 
+			PlayerHand(playerHand), 
+			Croupier(croupier),
+			IsHitted(false),
+			IsStanded(false),
+			IsDoubled(false),
+			IsSplited(false) {
+			if(baseBet < 0)
+				throw std::runtime_error("Bet can't be less than 0!");
+		}
+		Bet(float baseBet, Hand::Hand* playerHand, Croupier::Croupier* croupier, bool IsSplited) : BaseBet(baseBet), 
+			PlayerHand(playerHand), 
+			Croupier(croupier), 
+			IsHitted(false),
+			IsStanded(false),
+			IsDoubled(false),
+			IsSplited(IsSplited) {
+			if (baseBet < 0) 
+				throw std::runtime_error("Bet can't be less than 0!");
+		}
+		
+		float GetBaseBet();
+
+		float GetMultiplier();
+
+		void Hit();
+		bool CanHit();
+
+		void Stand();
+		bool CanStand();
+
+		void Double(Player::Player* player);
+		bool CanDouble(Player::Player* player);
+
+		Bet* Split(Player::Player* player);
+		bool CanSplit(Player::Player* player);
+
+		bool IsStop();
+
+		bool IsWin();
+		bool IsLost();
+		bool IsDraw();
+
+		float GetPay();
+
+		~Bet();
+	};
+}
